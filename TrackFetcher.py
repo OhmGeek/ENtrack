e4af40bbb117b8040ddff3ed155fa412d4644fbf
Ryan Collins <ryan_snowball@yahoo.co.uk>
2016-01-07 23:08:37 +0000

    Updated to use pyen
    This means that when used with other code it is far more reliable. loadInfo only gets information relating to the song ID specified from echonest.

--- TrackFetcher.py
+++ TrackFetcher.py
@@ -1,27 +1,11 @@
-import requests
+import json
+import pyen
+class SongInfo:
+    
+    def __init__(self, apikey):
+        self.en = pyen.Pyen(apikey)
+    #returns True if load successful, false if load problematic.
+    def loadInfo(self,ID):
+        response = self.en.get('track/profile',id=ID,bucket='audio_summary')
+        return response['track']['audio_summary']
 
-class TrackFetcher(object):
-	
-	def __init__(self, api_key):
-		self.api_key = api_key
-
-	def getInfo(self,ID):
-		rawurl = "http://developer.echonest.com/api/v4/track/profile"
-
-		#initialise parameters
-		par = {}
-		par["api_key"] = self.api_key
-		par["id"] = ID
-		par["format"] = "json"
-		par["bucket"] = "audio_summary"
-
-		#send get request and get JSON. Process this.
-		r = requests.get(rawurl, params = par)
-		trackInfo = r.json().get('response')
-		if(trackInfo is not None):
-			trackInfo = trackInfo.get('track')
-			if(trackInfo is not None):
-				trackInfo = trackInfo.get('audio_summary')
-				if(trackInfo is not None):
-					return trackInfo
-		return None
